cbf8275fcd7e1e050a634d97e78ff39d507015c6
Techtek-PI <smaris@gmail.com>
2017-08-27 20:12:22 +0000

    SteemPi v1.0  Webinterface + LED script

--- spacepi.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body bgcolor="#000000">
-
-<center><iframe src="http://www.ustream.tv/embed/17074538?html5ui=1&autoplay=true" style="border: 0 none transparent;" webkitallowfullscreen allowfullscreen frameborder="no" width="100%" height="214"></iframe></center>
-
-
-</body>
-</html>
