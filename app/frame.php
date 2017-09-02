4dd5bd9df4b387c84740d0b5c5abed79d530cf5f
Henning Leutz <leutz@pcsg.de>
2017-09-02 07:07:27 +0200

    refactor: main frame

--- app/frame.php
+++ app/frame.php
@@ -1,1 +1,21 @@
-<h1>Steem PI</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>STEEMPI | A system for Steemit</title>
+
+    <meta charset="utf-8"/>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1,minimum-scale=1,width=device-width">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+
+<div class="boot">
+    <img src="/app/images/logo.svg" class="boot-logo"/>
+    <img src="/app/images/logo-text.svg" class="boot-logo-text"/>
+</div>
+
+</body>
+</html>
