7c5831666b52e725bddbb36d0f882d6c2e2d7e65
Henning Leutz <leutz@pcsg.de>
2017-09-06 20:02:33 +0200

    feat: SteemPi CLI -> update, change-branch Options

--- steempi.php
+++ steempi.php
@@ -17,44 +17,87 @@ if (php_sapi_name() != 'cli') {
     die('Must run from command line');
 }
 
-error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL | E_ERROR | E_PARSE);
 
 ini_set('display_errors', 1);
 ini_set('log_errors', 0);
-ini_set('html_errors', 0);
 
 require_once 'app/autoload.php';
 
 $strict    = in_array('--strict', $_SERVER['argv']);
 $Arguments = new \cli\Arguments(compact('strict'));
 
+
 $Arguments->addFlag(array('help', 'h'), 'Show this help screen');
 $Arguments->addFlag(array('version', 'v'), 'Display the version');
-$Arguments->addFlag(array('update'), 'Update SteemPi');
+
+
+$Arguments->addOption('update', 'Update SteemPi');
+$Arguments->addOption('change-branch', 'Change between the branches from SteemPi: dev or master');
+
 
 $Arguments->parse();
+$arguments = json_decode($Arguments->asJSON(), true);
 
-if ($Arguments['help'] ||
-    (!$Arguments['update'] && !$Arguments['version'])
-) {
-    echo "
+$showHelp = function() use ($arguments) {
+    if (isset($arguments['help']) && $arguments['help']) {
+        return true;
+    }
+
+    foreach ($arguments as $name => $value) {
+        if (!empty($value)) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+if ($showHelp()) {
+$logo = "
 
   Welcome to
-  
+
    __ _                        ___ _
   / _\ |_ ___  ___ _ __ ___   / _ (_)
   \ \| __/ _ \/ _ \ '_ ` _ \ / /_)/ |
   _\ \ ||  __/  __/ | | | | / ___/| |
   \__/\__\___|\___|_| |_| |_\/    |_|
 
-
     ";
+
+    \cli\Colors::enable();
+    \cli\line('%C'. $logo .'%n', true);
+    \cli\Colors::disable();
+
+    echo PHP_EOL;
     echo $Arguments->getHelpScreen();
     echo PHP_EOL.PHP_EOL;
     exit;
 }
 
-if ($Arguments['update']) {
+if ($arguments['update']) {
     include 'app/bash/update.php';
     exit;
 }
+
+if (isset($arguments['change-branch'])) {
+    system('git fetch');
+    switch ($arguments['change-branch']) {
+        case 'dev':
+            system('git reset --hard origin/dev');
+            exit;
+
+        case 'master':
+            system('git reset --hard origin/master');
+            exit;
+
+        default:
+            \cli\Colors::enable();
+            \cli\line('Please use %C%5dev%n or %C%5master%n as value for --change-branch%n', true);
+            \cli\Colors::disable();
+            exit;
+    }
+}
+
+//var_dump($arguments);
