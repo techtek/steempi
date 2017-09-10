659a1d8819a9083a0ff035664d13471e1b162b16
Henning Leutz <leutz@pcsg.de>
2017-09-10 09:07:06 +0200

    feat: #24 - Add a Test LED button to settings -> GPIO test script

--- app/tests/gpio.php
+++ app/tests/gpio.php
@@ -4,26 +4,32 @@ require dirname(dirname(__FILE__)).'/autoload.php';
 
 use \SteemPi\GPIO;
 
+$gpio = 1;
+
+if (isset($argv[1])) {
+    $gpio = (int)$argv[1];
+}
+
 // on
-echo 'Set GPIO 1 on...';
+echo 'Set GPIO '.$gpio.' on...';
 GPIO::on(1);
 
 echo 'wait...';
 sleep(1);
 
-$status = GPIO::read(1);
+$status = GPIO::read($gpio);
 
 echo 'current status: ';
 echo $status.PHP_EOL;
 
 // off
-echo 'Set GPIO 1 off...';
-GPIO::off(1);
+echo 'Set GPIO '.$gpio.' off...';
+GPIO::off($gpio);
 
 echo 'wait...';
 sleep(1);
 
-$status = GPIO::read(1);
+$status = GPIO::read($gpio);
 
 echo 'current status: ';
 echo $status.PHP_EOL;
